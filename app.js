bd188d7872d21dac257ccd23b54cd13cf061a760
Rion Tono <gerrinkido@gmail.com>
2022-03-17 13:41:40 +0900

    demo

--- app.js
+++ app.js
@@ -23,23 +23,26 @@ async function run() {
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       console.log("Emit message", ++count);
-      // Emit an SSE that contains the current 'count' as a string
 
       if (count < 5) {
         res.write("retry: 10000\n\n");
         res.write(`id: test id: ${count}\n\n`);
-        // res.write("event: userdata\n");
-        res.write(`data: { \"hello\": message type ${count} } \n\n`);
+        // res.write("event: sselib_message\n");
+        res.write(`data: { \"hello\": default message type ${count} } \n\n`);
       } else if (count < 10) {
         res.write("retry: 10000\n\n");
         res.write(`id: test id: ${count}\n\n`);
         res.write("event: sselib_message\n");
-        res.write(`data: { \"hello\": userdata type ${count} } \n\n`);
+        res.write(
+          `data: { \"hello\": sselib_message message type ${count} } \n\n`
+        );
       } else if (count < 15) {
         res.write("retry: 10000\n\n");
         res.write(`id: test id: ${count}\n\n`);
         res.write("event: sselib_error\n");
-        res.write(`data: { \"hello\": error type ${count} } \n\n`);
+        res.write(
+          `data: { \"hello\": sselib_error message type ${count} } \n\n`
+        );
       } else {
         res.write("retry: 10000\n\n");
         res.write(`id: test id: ${count}\n\n`);
